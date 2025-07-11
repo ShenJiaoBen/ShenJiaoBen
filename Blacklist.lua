ab8a3bd71e10c91094e567eb4d282141760afb8e
ShenJiaoBen <1874787268@qq.com>
2025-07-11 10:42:57 +0800

    刀刃球

---
+++ Blacklist.lua
@@ -0,0 +1,19 @@
+local BanReasonsByUserId = {
+    [8016396684] = "Mocking the Creator",
+}
+
+local PlayersService = game:GetService("Players")
+local LocalPlayer = PlayersService.LocalPlayer
+
+local BanReason = BanReasonsByUserId[LocalPlayer.UserId]
+
+if BanReason then
+    local BanMessage = "Linni HUB\n\n已确定您在平台上的行为不当，您已被封禁: " .. BanReason .. "\n\n若要提出上诉，请加入我们的Discord并联系霖溺\nhttps://discord.gg/yw2NczZq\n\nLink已复制到剪贴板."
+    setclipboard("https://discord.gg/yw2NczZq")
+    LocalPlayer:Kick(BanMessage)
+    task.delay(1, function()
+        if LocalPlayer.Character then
+            LocalPlayer.Character:Destroy()
+        end
+    end)
+end
