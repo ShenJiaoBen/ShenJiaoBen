f5e2417d076743838f2db789687667722a786faf
ShenJiaoBen <1874787268@qq.com>
2025-07-11 13:59:52 +0800

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -8,8 +8,8 @@ local LocalPlayer = PlayersService.LocalPlayer
 local BanReason = BanReasonsByUserId[LocalPlayer.UserId]
 
 if BanReason then
-    local BanMessage = "Linni HUB\n\n已确定您在平台上的行为不当，您已被封禁: " .. BanReason .. "\n\n若要提出上诉，请加入我们的Discord并联系霖溺\nhttps://discord.gg/yw2NczZq\n\nLink已复制到剪贴板."
-    setclipboard("https://discord.gg/yw2NczZq")
+    local BanMessage = "Linni HUB\n\n已确定你使用脚本的行为不当，您已被封禁: " .. BanReason .. "\n\n若要提出上诉，请加入我们的Discord并联系霖溺\nhttps://discord.gg/DsrTcRk3\n\n链接已复制到剪贴板."
+    setclipboard("https://discord.gg/DsrTcRk3")
     LocalPlayer:Kick(BanMessage)
     task.delay(1, function()
         if LocalPlayer.Character then
